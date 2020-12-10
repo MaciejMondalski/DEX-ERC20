3443704f31c44605a9a0f5b13650ce231d2f403c
Maciej Mondalski <maciej.mondalski@gmail.com>
2020-12-10 21:52:57 +0100

    create smart contracts

---
+++ contracts/mocks/Bat.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Bat is ERC20 {
+    constructor() public ERC20("BAT", "Brave browser token") {}
+
+    function faucet(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
---
+++ contracts/mocks/Dai.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Dai is ERC20 {
+    constructor() public ERC20("DAI", "Dai stableCoin") {}
+
+    function faucet(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
